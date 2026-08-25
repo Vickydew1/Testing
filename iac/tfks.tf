8968097eb783719857592e63351b1a6841a55511
Vicky Dewangan <dewanganvicky2@gmail.com>
2026-08-25 10:44:31 +0530

    Update iac/tfks.tf
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- iac/tfks.tf
+++ iac/tfks.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_eks_cluster" "demo" {
-  name     = "demo-cluster"
+enabled_cluster_log_types = ["api", "audit"]
   role_arn = "arn:aws:iam::123456789012:role/demo-role"
 
   vpc_config {
